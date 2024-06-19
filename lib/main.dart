9d2222f9560cb92abd4424e09bff4d5cab4ea869
FrankFan <frankfancode@gmail.com>
2024-06-19 17:17:05 +0800

    show sliverconstraint and geomertry

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
+import 'dart:math';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,113 +18,341 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+enum GeometryPaintOriginType { asDefault, absorb, custom }
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+GeometryPaintOriginType geometryPaintOriginType =
+    GeometryPaintOriginType.asDefault;
+
+class _MyHomePageState extends State<MyHomePage> {
+  bool appbarPinned = true;
+  bool appbarFloat = true;
+  double toolBarHeight = 56;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    return Material(
+      child: Row(
+        children: [
+          Container(
+            width: 375,
+            child: NotificationListener(
+              onNotification: (notification) {
+                setState(() {});
+                return false;
+              },
+              child: CustomScrollView(
+                slivers: [
+                  SliverAppBar(
+                    pinned: appbarPinned,
+                    floating: appbarFloat,
+                    toolbarHeight: toolBarHeight,
+                    title: LayoutBuilder(
+                      builder:
+                          (BuildContext context, BoxConstraints constraints) {
+                        return Text('SliverAppBar height $toolBarHeight');
+                      },
+                    ),
+                  ),
+                  SliverToBoxAdapter(
+                    child: Container(
+                      height: 100,
+                      color: Colors.blue.shade100,
+                      child: const Text('SliverToBoxAdapter, Height 100.0'),
+                    ),
+                  ),
+                  MySliverToBoxAdapter(
+                    child: Builder(builder: (context) {
+                      return Container(
+                        color: Colors.amber,
+                        height: 100,
+                        child: const Text('MySliverToBoxAdapter height 100.0'),
+                      );
+                    }),
+                  ),
+                  SliverLayoutBuilder(
+                    builder: (context, constraints) {
+                      ConstraintsData.sliverPinnedConstraints = constraints;
+                      listenedPinnedsliverGeometry =
+                          findRenderSliverPersistentHeaders(context);
+
+                      return SliverPersistentHeader(
+                        pinned: true,
+                        delegate: MySliverPersistentHeaderDelegate(
+                            child: Container(
+                                color: Colors.pinkAccent.withOpacity(0.8),
+                                child: Text(
+                                    'pinned header maxHeight 100, minHeight 30')),
+                            maxHeight: 100,
+                            minHeight: 30),
+                      );
+                    },
+                  ),
+                  SliverList.builder(
+                    itemBuilder: (context, index) {
+                      return Text('data $index');
+                    },
+                  )
+                ],
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          ),
+          const VerticalDivider(),
+          SizedBox(
+            width: 375,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  'Total',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                ),
+                Text(
+                  'viewport width ${MediaQuery.of(context).size.width} height ${MediaQuery.of(context).size.height}',
+                ),
+                const Text(
+                  'SliverAppBar',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                ),
+                Row(children: [
+                  const Text('pinned'),
+                  Checkbox(
+                    value: appbarPinned,
+                    onChanged: (value) {
+                      setState(() {
+                        appbarPinned = value!;
+                      });
+                    },
+                  ),
+                  const Text('float'),
+                  Checkbox(
+                    value: appbarFloat,
+                    onChanged: (value) {
+                      setState(() {
+                        appbarFloat = value!;
+                      });
+                    },
+                  )
+                ]),
+                const Text(
+                  'SliverToBoxAdapter',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                ),
+                const Text(
+                  'SliverConstarint',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                ),
+                _buildSliverConstrainsWidget(
+                    ConstraintsData.sliverBoxConstraints),
+                const Text(
+                  'Geometry',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                ),
+                _buildGeometry(listenedsliverGeometry),
+              ],
             ),
-          ],
-        ),
+          ),
+          const VerticalDivider(),
+          SizedBox(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  'SliverPinned',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                ),
+                const Text(
+                  'SliverConstarint',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                ),
+                _buildSliverConstrainsWidget(
+                    ConstraintsData.sliverPinnedConstraints),
+                const Text(
+                  'Geometry',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                ),
+                _buildGeometry(listenedPinnedsliverGeometry),
+              ],
+            ),
+          )
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  Widget _buildSliverConstrainsWidget(SliverConstraints? sliverConstraints) {
+    return Wrap(
+      direction: Axis.vertical,
+      spacing: 2,
+      children: [
+        Text('axisDirection:${sliverConstraints?.axisDirection}'),
+        Text('crossAxisDirection:${sliverConstraints?.crossAxisDirection}'),
+        Text('growthDirection:${sliverConstraints?.growthDirection}'),
+        Text(
+            'viewportMainAxisExtent:${sliverConstraints?.viewportMainAxisExtent}'),
+        Text('crossAxisExtent:${sliverConstraints?.crossAxisExtent}'),
+        Text(
+            'precedingScrollExtent:${sliverConstraints?.precedingScrollExtent}'),
+        Text('userScrollDirection:${sliverConstraints?.userScrollDirection}'),
+        Text('remainingPaintExtent:${sliverConstraints?.remainingPaintExtent}'),
+        Text('remainingCacheExtent:${sliverConstraints?.remainingCacheExtent}'),
+        Text('overlap:${sliverConstraints?.overlap}'),
+        Text('cacheOrigin:${sliverConstraints?.cacheOrigin}'),
+        Text('scrollOffset:${sliverConstraints?.scrollOffset}'),
+      ],
+    );
+  }
+
+  Widget _buildGeometry(SliverGeometry? geometry) {
+    return Wrap(
+      direction: Axis.vertical,
+      spacing: 2,
+      children: [
+        Text('scrollExtent:${geometry?.scrollExtent}'),
+        Text('paintExtent: ${geometry?.paintExtent}'),
+        Text('paintOrigin: ${geometry?.paintOrigin}'),
+        Text('layoutExtent: ${geometry?.layoutExtent}'),
+        Text('maxPaintExtent: ${geometry?.maxPaintExtent}'),
+        Text(
+            'maxScrollObstructionExtent: ${geometry?.maxScrollObstructionExtent}'),
+        Text('crossAxisExtent: ${geometry?.crossAxisExtent}'),
+        Text('hitTestExtent: ${geometry?.hitTestExtent}'),
+        Text('visible: ${geometry?.visible}'),
+        Text('hasVisualOverflow: ${geometry?.hasVisualOverflow}'),
+        Text('cacheExtent: ${geometry?.cacheExtent}'),
+      ],
+    );
+  }
+
+  SliverGeometry? findRenderSliverPersistentHeaders(BuildContext context) {
+    SliverGeometry? geometry;
+    SliverGeometry? visitor(RenderObject child) {
+      if (child is RenderSliverPersistentHeader) {
+        geometry = child.geometry;
+      }
+      child.visitChildren(visitor);
+      return null;
+    }
+
+    final rootRenderObject = context.findRenderObject();
+    if (rootRenderObject != null) {
+      rootRenderObject.visitChildren(visitor);
+    }
+    return geometry;
+  }
+}
+
+/// copy from [[SliverToBoxAdapter]]
+class MySliverToBoxAdapter extends SingleChildRenderObjectWidget {
+  /// Creates a sliver that contains a single box widget.
+  const MySliverToBoxAdapter({
+    super.key,
+    super.child,
+  });
+
+  @override
+  MyRenderSliverToBoxAdapter createRenderObject(BuildContext context) =>
+      MyRenderSliverToBoxAdapter();
+}
+
+/// copy from [RenderSliverToBoxAdapter]
+class MyRenderSliverToBoxAdapter extends RenderSliverSingleBoxAdapter {
+  /// Creates a [RenderSliver] that wraps a [RenderBox].
+  MyRenderSliverToBoxAdapter({
+    super.child,
+  });
+
+  @override
+  void performLayout() {
+    if (child == null) {
+      geometry = SliverGeometry.zero;
+      return;
+    }
+
+    final SliverConstraints constraints = this.constraints;
+    ConstraintsData.sliverBoxConstraints = this.constraints;
+    child!.layout(constraints.asBoxConstraints(), parentUsesSize: true);
+    final double childExtent;
+    switch (constraints.axis) {
+      case Axis.horizontal:
+        childExtent = child!.size.width;
+      case Axis.vertical:
+        childExtent = child!.size.height;
+    }
+    final double paintedChildSize =
+        calculatePaintOffset(constraints, from: 0.0, to: childExtent);
+    final double cacheExtent =
+        calculateCacheOffset(constraints, from: 0.0, to: childExtent);
+
+    assert(paintedChildSize.isFinite);
+    assert(paintedChildSize >= 0.0);
+    geometry = SliverGeometry(
+      scrollExtent: childExtent,
+      paintExtent: paintedChildSize,
+      cacheExtent: cacheExtent,
+      maxPaintExtent: childExtent,
+      hitTestExtent: paintedChildSize,
+      hasVisualOverflow: childExtent > constraints.remainingPaintExtent ||
+          constraints.scrollOffset > 0.0,
+    );
+    setChildParentData(child!, constraints, geometry!);
+    listenedsliverGeometry = geometry;
+  }
+}
+
+class ConstaintsNotification extends Notification {
+  ConstaintsNotification(this.constraints);
+  final SliverConstraints constraints;
+}
+
+class ConstraintsData {
+  static SliverConstraints? sliverBoxConstraints;
+  static SliverConstraints? sliverPinnedConstraints;
+}
+
+SliverGeometry? listenedsliverGeometry;
+SliverGeometry? listenedPinnedsliverGeometry;
+
+class MySliverPersistentHeaderDelegate extends SliverPersistentHeaderDelegate {
+  MySliverPersistentHeaderDelegate({
+    required this.minHeight,
+    required this.maxHeight,
+    required this.child,
+  });
+  final double minHeight;
+  final double maxHeight;
+  final Widget child;
+
+  @override
+  double get minExtent => minHeight;
+  @override
+  double get maxExtent => max(maxHeight, minHeight);
+
+  @override
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return new SizedBox.expand(child: child);
+  }
+
+  @override
+  bool shouldRebuild(MySliverPersistentHeaderDelegate oldDelegate) {
+    return maxHeight != oldDelegate.maxHeight ||
+        minHeight != oldDelegate.minHeight ||
+        child != oldDelegate.child;
   }
 }
